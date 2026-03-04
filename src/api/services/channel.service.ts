0df4504a11d535c0ec2a1f3ef3652ab4daa7cdd4
Vitor Duggen <zduggem@gmail.com>
2026-03-04 15:54:13 -0300

    refactor(channel): remove unused pushName field

--- src/api/services/channel.service.ts
+++ src/api/services/channel.service.ts
@@ -818,7 +818,6 @@ export class ChannelStartupService {
               to_timestamp("Message"."messageTimestamp"::double precision),
               "Contact"."updatedAt"
             ) as "updatedAt",
-            "Chat"."name" as "pushName",
             "Chat"."createdAt" as "windowStart",
             "Chat"."createdAt" + INTERVAL '24 hours' as "windowExpires",
             "Chat"."unreadMessages" as "unreadMessages",
