38f850171b1cfc9981acfa1fede4154aad7c9322
agenciamtd10-lab <agenciamtd10@gmail.com>
2026-05-21 00:17:38 -0300

    Update start.js

--- start.js
+++ start.js
@@ -1,2 +1,13 @@
-process.env.SERVER_PORT = process.env.PORT || process.env.SERVER_PORT || '8080';
+process.on('uncaughtException', (err) => {
+  require('fs').appendFileSync('/tmp/evo-error.log', new Date() + '\n' + err.stack + '\n\n');
+});
+process.on('unhandledRejection', (reason) => {
+  require('fs').appendFileSync('/tmp/evo-error.log', new Date() + '\n' + String(reason) + '\n\n');
+});
+
+const dbPass = encodeURIComponent('121000aA@#');
+process.env.DATABASE_CONNECTION_URI = `mysql://u229800555_admin_aviva:${dbPass}@127.0.0.1:3306/u229800555_evolution`;
+process.env.DATABASE_ENABLED = 'true';
+process.env.DATABASE_PROVIDER = 'mysql';
+
 require('./dist/main.js');
