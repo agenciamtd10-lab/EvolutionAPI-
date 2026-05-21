82423e26f23c01eaf7f9ebc9ec6e7ad7f500a0bb
agenciamtd10-lab <agenciamtd10@gmail.com>
2026-05-21 00:22:05 -0300

    Update start.js

--- start.js
+++ start.js
@@ -9,5 +9,6 @@ const dbPass = encodeURIComponent('121000aA@#');
 process.env.DATABASE_CONNECTION_URI = `mysql://u229800555_admin_aviva:${dbPass}@127.0.0.1:3306/u229800555_evolution`;
 process.env.DATABASE_ENABLED = 'true';
 process.env.DATABASE_PROVIDER = 'mysql';
+process.env.AUTHENTICATION_API_KEY = 'InspireAds2026EvolutionKey';
 
 require('./dist/main.js');
